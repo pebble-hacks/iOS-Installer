b18c2276017c84081421b3f84476cc468a312551
Thomas Sarlandie <thomas@sarlandie.net>
2014-01-31 17:12:53 -0800

    Fix crash: we need to retain the UIDocumentInteractionController

--- PebbleInstaller/PIInstallerViewController.m
+++ PebbleInstaller/PIInstallerViewController.m
@@ -10,6 +10,8 @@
 
 @interface PIInstallerViewController ()
 
+@property (strong) UIDocumentInteractionController *uidocumentInteractionController;
+
 @end
 
 @implementation PIInstallerViewController
@@ -17,16 +19,16 @@
 - (void) installPebbleApplication
 {
     NSURL *pbwUrl = [[NSBundle mainBundle] URLForResource:@"simplicity" withExtension:@"pbw"];
-    UIDocumentInteractionController *docController = [UIDocumentInteractionController interactionControllerWithURL:pbwUrl];
-    docController.UTI = @"com.getpebble.bundle.watchface";
-    docController.delegate = self;
+    self.uidocumentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:pbwUrl];
+    self.uidocumentInteractionController.UTI = @"com.getpebble.bundle.watchface";
+    self.uidocumentInteractionController.delegate = self;
     
     // presentOptionsMenu works all the time
-    //[docController presentOptionsMenuFromRect:self.view.frame inView:self.view animated:YES];
+    //[self.uidocumentInteractionController presentOptionsMenuFromRect:self.view.frame inView:self.view animated:YES];
     
     // presentOpenInMenu is better (only shows compatible apps) but only works
     // if the Pebble app is installed (so not in the simulator)
-    [docController presentOpenInMenuFromRect:self.view.frame inView:self.view animated:YES];
+    [self.uidocumentInteractionController presentOpenInMenuFromRect:self.view.frame inView:self.view animated:YES];
     
     // For more information see presentOpenInMenuFromRect documentation:
     // https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIDocumentInteractionController_class/Reference/Reference.html#//apple_ref/doc/uid/TP40009304-CH1-SW13
@@ -36,8 +38,5 @@
 {
     [self installPebbleApplication];
 }
-
-
-
 
 @end
